39ab37d9b0a0d6a719fa26f53abc1866c2b4a1ab
benny <bbbsnowball@gmail.com>
2012-07-27 13:43:25 +0200

    updated feature config for all projects to match the generated one

--- XMEGAOs/test_application/gen/Os_cfg_features.h
+++ XMEGAOs/test_application/gen/Os_cfg_features.h
@@ -1,5 +1,5 @@
 /*
- * OS_features.h
+ * Os_cfg_features.h
  *
  * This file defines the features the the OS should have. Whenever you change
  * it, you must recompile the OS library. The library name should contain the
@@ -9,11 +9,15 @@
 #ifndef OS_CFG_FEATURES_H_
 #define OS_CFG_FEATURES_H_
 
+#ifndef __AVR_AT90CAN128__
+#	error Expecting AT90CAN128. Either update your project configuration / Makefile or the code generator configuration.
+#endif
+
 #define OS_CFG_CC BCC1
 
 //TODO put as much as possible into program memory constants or
 //     initialization functions in OS_cfg_generated.c
-#define OS_TIMER_PRESCALE			TIMER_PRESCALE_1_bm
-#define OS_TIMER_COMPARE_VALUE		0x7D00
+#define OS_TIMER_PRESCALE			TIMER_PRESCALE_64_bm
+#define OS_TIMER_COMPARE_VALUE		12499
 
 #endif /* OS_CFG_FEATURES_H_ */
