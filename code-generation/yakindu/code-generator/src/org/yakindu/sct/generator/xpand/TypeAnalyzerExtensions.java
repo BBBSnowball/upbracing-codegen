ec37c80bfecc0f0f50af134a9f3e609a17c7a61f
Rishab Dhar <rishabdhar1988@gmail.com>
2012-08-09 22:12:47 +0200

    FSM: demonstrate custom code generation from Yakindu state machine; written by Rishab, committed by Benjamin; IMPORTANT: You have to use the Yakindu release that is mentioned in the specification paper.

---
+++ code-generation/yakindu/code-generator/src/org/yakindu/sct/generator/xpand/TypeAnalyzerExtensions.java
@@ -0,0 +1,54 @@
+package org.yakindu.sct.generator.xpand;
+
+import org.eclipse.xtend.expression.ExecutionContext;
+import org.eclipse.xtend.expression.IExecutionContextAware;
+import org.yakindu.base.types.ITypeSystemAccess;
+import org.yakindu.base.types.Type;
+import org.yakindu.sct.model.sgraph.Statement;
+import org.yakindu.sct.model.stext.validation.ITypeInferrer;
+
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+
+public class TypeAnalyzerExtensions implements IExecutionContextAware {
+
+	private static final Object CONTEXT_INJECTOR_PROPERTY_NAME = "AbstractXpandBasedCodeGenerator.Injector";
+	@Inject
+	private ITypeInferrer typeInferrer;
+	@Inject
+	private ITypeSystemAccess access;
+
+	public void setExecutionContext(ExecutionContext ctx) {
+		Injector injector = null;
+		if (ctx.getGlobalVariables().get(CONTEXT_INJECTOR_PROPERTY_NAME) != null) {
+			injector = (Injector) ctx.getGlobalVariables()
+					.get(CONTEXT_INJECTOR_PROPERTY_NAME).getValue();
+		} else if (ctx.getGlobalVariables().get(Injector.class.getName()) != null) {
+			injector = (Injector) ctx.getGlobalVariables()
+					.get(Injector.class.getName()).getValue();
+		}
+		if (injector != null) {
+			injector.injectMembers(this);
+		}
+	}
+
+	public boolean isBoolean(Type type) {
+		return access.isBoolean(type);
+	}
+
+	public boolean isInteger(Type type) {
+		return access.isInteger(type);
+	}
+
+	public boolean isReal(Type type) {
+		return access.isReal(type);
+	}
+
+	public boolean isString(Type type) {
+		return access.isString(type);
+	}
+
+	public Type getType(Statement stmt) {
+		return typeInferrer.getType(stmt);
+	}
+}
