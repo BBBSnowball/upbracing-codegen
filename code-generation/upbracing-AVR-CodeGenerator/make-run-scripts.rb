36e3f8b06a4b9145ffcb1519f8687bfda1c5d4d6
benny <bbbsnowball@gmail.com>
2012-09-11 17:33:00 +0200

    code-generation: run scripts support dependent projects

--- code-generation/upbracing-AVR-CodeGenerator/make-run-scripts.rb
+++ code-generation/upbracing-AVR-CodeGenerator/make-run-scripts.rb
@@ -1,21 +1,63 @@
 require 'rubygems'
 require 'xmlsimple'
 
-filename = File.dirname(__FILE__) + "/.classpath"
-data = XmlSimple.xml_in(filename)
-classpath = []
-ecus = data["classpathentry"].each do |entry|
-  case entry["kind"]
-  when "con"
-    #ignore
-  when "src"
-    classpath << entry["output"] if entry["output"]
-  when "output"
-    classpath.insert(0, entry["path"])
-  when "lib"
-    classpath << entry["path"]
+def relative_path(parent, child)
+  if File.absolute_path(child) != child
+    File.join(parent, child)
+  else
+    child
   end
 end
+
+def read_classpath(filename, referenced_projects)
+  data = XmlSimple.xml_in(filename)
+  classpath = []
+  data["classpathentry"].each do |entry|
+    case entry["kind"]
+    when "con"
+      #ignore
+    when "src"
+      if entry["path"] && entry["path"].start_with?("/")
+        # this is a project
+        project_name = entry["path"][1..-1]
+        
+        projects = referenced_projects[project_name]
+        if projects
+          # load classpath of referenced project and add it
+          projects = [projects] unless projects.is_a? Array 
+          projects = projects.map {|x| File.join(x, ".classpath")}
+          referenced_project_classpath_file = projects.first {|x| File.exists? x} 
+          
+          if referenced_project_classpath_file
+            referenced_project_classpath = read_classpath(referenced_project_classpath_file,
+                                                          referenced_projects)
+            referenced_project_classpath = referenced_project_classpath.map {|x|
+              relative_path(File.dirname(referenced_project_classpath_file), x) }
+            #TODO remove duplicates
+            classpath.concat(referenced_project_classpath)
+          else
+            puts "WARN: Found some paths for '{project_name}', but none of them exists"
+          end
+        else
+          puts "WARN: ignoring referenced project '#{project_name}' because I don't know where it is"
+        end
+      else
+        # it's a source folder
+        classpath << entry["output"] if entry["output"]
+      end
+    when "output"
+      classpath.insert(0, entry["path"])
+    when "lib"
+      classpath << entry["path"]
+    end
+  end
+  return classpath
+end
+
+dir = File.dirname(__FILE__)
+filename = File.join(dir, ".classpath")
+classpath = read_classpath(filename,
+  "statemachine" => File.join(dir, "../StatechartEditor"))
 
 unix_classpath = classpath.map {|x| '$DIR/' + x}.join ":"
 win_classpath = classpath.map {|x| '%~dp0\\' + x}.join ";"
