22673872cecbc9e72da3e362a58f9076a1641b43
Peer Adelt <adelt@mail.uni-paderborn.de>
2012-09-25 22:14:09 +0200

    Forgot something important during last commit. Still fix of "TerminateTask". Should now work with any optimization level.

--- XMEGAOs/os/internal/OSEK_Task.c
+++ XMEGAOs/os/internal/OSEK_Task.c
@@ -11,27 +11,18 @@
 
 // Terminates the running task
 // Never returns.
-StatusType TerminateTask(void) 
+StatusType volatile TerminateTask(void) 
 {	
+	//OS_ENTER_CRITICAL();
+	
 	//QUESTION(Benjamin): Why does a local variable have to be volatile?
-	//volatile uint8_t taskAddressLow = 0, taskAddressHigh = 0;
-	volatile uint16_t taskAddress = 0;
-	volatile StackPointerType *sp = os_currentTcb->topOfStack;
+	//Note(Peer): There was no good reason. Removed the keyword
+	uint16_t taskAddress = 0;
+	StackPointerType *sp = os_currentTcb->topOfStack;
 
 	// Set state to suspended
 	os_currentTcb->state = SUSPENDED;
 	os_currentTcb->currentBaseOfStack = os_currentTcb->topOfStack;
-	
-	//QUESTION(Benjamin): The compiler may be using r26-r29. Can we overwrite
-	//                    them without further precautions? You could let the
-	//                    compiler choose the registers.
-	//QUESTION(Benjamin): Why to you use assembly code? Is it because we have to change SP?	
-	/*asm volatile(	"lds r26, os_currentTcb		\n\t"	\
-					//"lds r27, os_currentTcb + 1	\n\t"	\
-					//"ld r28, x+					\n\t"	\
-					//"out __SP_L__, r28			\n\t"	\
-					//"ld r29, x+					\n\t"	\
-					//"out __SP_H__, r29			\n\t"	);*/
 	
 	/* Reset return address to os_currentTcb->func */
 	//QUESTION(Benjamin): What "return address" do we set here? Is it the instruction pointer
@@ -44,85 +35,60 @@ StatusType TerminateTask(void)
 	*sp = (StackPointerType) ( taskAddress & ( uint16_t ) 0x00ff );
 	sp--;
 	
-	/* R0, then the status register */
-	*sp = (uint8_t) 0x00;								// R0
-	sp--;
-	*sp = Os_STATUS_REG_INT_ENABLED;					// SREG with global interrupt enable flag
-	sp--;
+	// The rest is written in assembly to ensure, that no optimizations take place here.
+	/* Set StackPointer to sp */
+	asm volatile(	"lds r26, os_currentTcb		\n\t"
+					"lds r27, os_currentTcb + 1	\n\t"
+					"ld r28, x+					\n\t"
+					"dec r28					\n\t"
+					"dec r28					\n\t"
+					"out __SP_L__, r28			\n\t"
+					"ld r29, x+					\n\t"
+					"out __SP_H__, r29			\n\t"
 	
-	/* R1 up to R31 */
-	//QUESTION(Benjamin): Are those the initial values for the registers? Can't we just set them to zero?
-	*sp = (uint8_t) 0x00;								// R1
-	sp--;
-	*sp = (uint8_t) 0x02;								// R2
-	sp--;
-	*sp = (uint8_t) 0x03;								// R3
-	sp--;
-	*sp = (uint8_t) 0x04;								// R4
-	sp--;
-	*sp = (uint8_t) 0x05;								// R5
-	sp--;
-	*sp = (uint8_t) 0x06;								// R6
-	sp--;
-	*sp = (uint8_t) 0x07;								// R7
-	sp--;
-	*sp = (uint8_t) 0x08;								// R8
-	sp--;
-	*sp = (uint8_t) 0x09;								// R9
-	sp--;
-	*sp = (uint8_t) 0x10;								// R10
-	sp--;
-	*sp = (uint8_t) 0x11;								// R11
-	sp--;
-	*sp = (uint8_t) 0x12;								// R12
-	sp--;
-	*sp = (uint8_t) 0x13;								// R13
-	sp--;
-	*sp = (uint8_t) 0x14;								// R14
-	sp--;
-	*sp = (uint8_t) 0x15;								// R15
-	sp--;
-	*sp = (uint8_t) 0x16;								// R16
-	sp--;
-	*sp = (uint8_t) 0x17;								// R17
-	sp--;
-	*sp = (uint8_t) 0x18;								// R18
-	sp--;
-	*sp = (uint8_t) 0x19;								// R19
-	sp--;
-	*sp = (uint8_t) 0x20;								// R20
-	sp--;
-	*sp = (uint8_t) 0x21;								// R21
-	sp--;
-	*sp = (uint8_t) 0x22;								// R22
-	sp--;
-	*sp = (uint8_t) 0x23;								// R23
-	sp--;
-	*sp = (uint8_t) 0x00;								// R24
-	sp--;
-	*sp = (uint8_t) 0x00;								// R25
-	sp--;
-	*sp = (uint8_t) 0x26;								// R26
-	sp--;
-	*sp = (uint8_t) 0x27;								// R27
-	sp--;
-	*sp = (uint8_t) 0x28;								// R28
-	sp--;
-	*sp = (uint8_t) 0x29;								// R29
-	sp--;
-	*sp = (uint8_t) 0x30;								// R30
-	sp--;
-	*sp = (uint8_t) 0x31;								// R31
-	sp--;
-		
+	/* Clear R0, then reset the status register, then clear following registers */
+		// Push Zero for R0
+		"clr r1									\n\t"
+		"push r1								\n\t"
+		// Push init SREG
+		"ldi r16, 128		\n\t"
+		"push r16								\n\t"
+		// Push Zero for R1-R31
+		"push r1								\n\t"
+		"push r1								\n\t"
+		"push r1								\n\t"
+		"push r1								\n\t"
+		"push r1								\n\t"
+		"push r1								\n\t"
+		"push r1								\n\t"
+		"push r1								\n\t"
+		"push r1								\n\t"
+		"push r1								\n\t"
+		"push r1								\n\t"
+		"push r1								\n\t"
+		"push r1								\n\t"
+		"push r1								\n\t"
+		"push r1								\n\t"
+		"push r1								\n\t"
+		"push r1								\n\t"
+		"push r1								\n\t"
+		"push r1								\n\t"
+		"push r1								\n\t"
+		"push r1								\n\t"
+		"push r1								\n\t"
+		"push r1								\n\t"
+		"push r1								\n\t"
+		"push r1								\n\t"
+		"push r1								\n\t"
+		"push r1								\n\t"
+		"push r1								\n\t"
+		"push r1								\n\t"
+		"push r1								\n\t"
+		"push r1								\n\t"
+	);
+	
+	// Set the correct stack address in the TCB for later restore		
 	os_currentTcb->currentBaseOfStack = os_currentTcb->topOfStack - 35;
-	
-	asm volatile(	"lds r26, os_currentTcb		\n\t"	\
-					"lds r27, os_currentTcb + 1	\n\t"	\
-					"ld r28, x+					\n\t"	\
-					"out __SP_L__, r28			\n\t"	\
-					"ld r29, x+					\n\t"	\
-					"out __SP_H__, r29			\n\t"	);
 
 	// Switch to another task
 	Os_Schedule();
