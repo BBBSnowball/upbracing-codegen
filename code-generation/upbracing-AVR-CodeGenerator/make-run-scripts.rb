da5658f54ad79d72b654a92c96614d6207723eb5
benny <bbbsnowball@gmail.com>
2012-10-05 22:44:01 +0200

    code-generation: make-run-scripts.rb can handle the reference to Peer's timer jar

--- code-generation/upbracing-AVR-CodeGenerator/make-run-scripts.rb
+++ code-generation/upbracing-AVR-CodeGenerator/make-run-scripts.rb
@@ -9,6 +9,36 @@ def relative_path(parent, child)
   end
 end
 
+def lookup_path(path, referenced_projects, sub_path = nil)
+  if path && path.start_with?("/")
+    # the path is absolute -> we must look it up in the workspace
+    parts = path.match(/^\/+([^\/]+)(\/(.*))?$/)
+    project_name = parts[1]
+    path_in_project = parts[3]
+    
+    projects = referenced_projects[project_name]
+    if projects
+      projects = [projects] unless projects.is_a? Array
+      projects = projects.map {|x| File.join(x, path_in_project)}  if path_in_project
+      projects = projects.map {|x| File.join(x, sub_path)}  if sub_path
+      referenced_project_sub_file = projects.first {|x| File.exists? x}
+      
+      if referenced_project_sub_file
+        return referenced_project_sub_file
+      else
+        puts "WARN: Found some paths for '{project_name}', but none of them exists"
+        return nil
+      end
+    else
+      puts "WARN: ignoring referenced project '#{project_name}' because I don't know where it is"
+        return nil
+    end
+  else
+    # it's a relative path -> do not change it
+    return path
+  end
+end
+
 def read_classpath(filename, referenced_projects)
   data = XmlSimple.xml_in(filename)
   classpath = []
@@ -19,27 +49,18 @@ def read_classpath(filename, referenced_projects)
     when "src"
       if entry["path"] && entry["path"].start_with?("/")
         # this is a project
-        project_name = entry["path"][1..-1]
-        
-        projects = referenced_projects[project_name]
-        if projects
-          # load classpath of referenced project and add it
-          projects = [projects] unless projects.is_a? Array 
-          projects = projects.map {|x| File.join(x, ".classpath")}
-          referenced_project_classpath_file = projects.first {|x| File.exists? x} 
-          
-          if referenced_project_classpath_file
-            referenced_project_classpath = read_classpath(referenced_project_classpath_file,
-                                                          referenced_projects)
-            referenced_project_classpath = referenced_project_classpath.map {|x|
-              relative_path(File.dirname(referenced_project_classpath_file), x) }
-            #TODO remove duplicates
-            classpath.concat(referenced_project_classpath)
-          else
-            puts "WARN: Found some paths for '{project_name}', but none of them exists"
-          end
-        else
-          puts "WARN: ignoring referenced project '#{project_name}' because I don't know where it is"
+
+        # lookup path and by that find the .classpath file
+        referenced_project_classpath_file = lookup_path(entry["path"], referenced_projects, ".classpath")
+
+        # load classpath of referenced project and add it
+        if referenced_project_classpath_file
+          referenced_project_classpath = read_classpath(referenced_project_classpath_file,
+                                                        referenced_projects)
+          referenced_project_classpath = referenced_project_classpath.map {|x|
+            relative_path(File.dirname(referenced_project_classpath_file), x) }
+          #TODO remove duplicates
+          classpath.concat(referenced_project_classpath)
         end
       else
         # it's a source folder
@@ -48,7 +69,8 @@ def read_classpath(filename, referenced_projects)
     when "output"
       classpath.insert(0, entry["path"])
     when "lib"
-      classpath << entry["path"]
+      path = lookup_path(entry["path"], referenced_projects)
+      classpath << path   if path
     end
   end
   return classpath
@@ -58,10 +80,11 @@ dir = File.dirname(__FILE__)
 filename = File.join(dir, ".classpath")
 classpath = read_classpath(filename,
   "statemachine" => File.join(dir, "../StatechartEditor"),
-  "Parser" => File.join(dir, "../java-parser-tools"))
+  "Parser" => File.join(dir, "../java-parser-tools"),
+  "upbracing-AVR-TimerConfigurationModel" => File.join(dir, "../upbracing-AVR-TimerConfigurationModel"))
 
 unix_classpath = classpath.map {|x| '$DIR/' + x}.join ":"
-win_classpath = classpath.map {|x| '%~dp0\\' + x}.join ";"
+win_classpath = classpath.map {|x| '%~dp0\\' + x.gsub("/", "\\")}.join ";"
 
 unix_classpath += ":$EXTRA_CLASSPATH"
 win_classpath  += ";%EXTRA_CLASSPATH%"
