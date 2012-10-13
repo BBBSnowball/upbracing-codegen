0b6bb4fb4d71748417289671fc10cfd7886dd5e9
benny <bbbsnowball@gmail.com>
2012-10-13 16:16:46 +0200

    code-generation - simple: using common.h in upbracing-common instead of generating a new one

--- code-generation/upbracing-AVR-CodeGenerator/src/de/upbracing/code_generation/generators/CommonHeaderGenerator.java
+++
@@ -1,9 +0,0 @@
-package de.upbracing.code_generation.generators;
-
-import de.upbracing.code_generation.CommonHeaderTemplate;
-
-public class CommonHeaderGenerator extends AbstractGenerator {
-	public CommonHeaderGenerator() {
-		super("common.h", new CommonHeaderTemplate());
-	}
-}
--- code-generation/upbracing-AVR-CodeGenerator/tests/de/upbracing/code_generation/test/AllTests.java
+++ code-generation/upbracing-AVR-CodeGenerator/tests/de/upbracing/code_generation/test/AllTests.java
@@ -9,8 +9,7 @@ import org.junit.runners.Suite.SuiteClasses;
 		TestGlobalVariableGenerator.class, TestPinGenerator.class,
 		TestRTOSGenerator.class, TestStatemachineGenerator.class,
 		TestTable.class, TestStateNameValidation.class,
-		TestMessages.class, TestUtils.class,
-		TestCommonHeaderGenerator.class })
+		TestMessages.class, TestUtils.class })
 public class AllTests {
 
 }
