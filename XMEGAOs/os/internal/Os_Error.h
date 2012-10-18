39f61c779ebd3ded33d76c0e88e98e6caa184bb0
benny <bbbsnowball@gmail.com>
2012-10-19 01:41:15 +0200

    implementation of error handling helpers

--- XMEGAOs/os/internal/Os_Error.h
+++ XMEGAOs/os/internal/Os_Error.h
@@ -20,13 +20,38 @@ void OS_error(OS_ERROR_CODE error);
 
 // Report an error (of any severity). Used by the OS.
 // It will disable interrupts before it calls OS_error.
-void OS_report_error(OS_ERROR_CODE error);
+inline static void OS_report_error(OS_ERROR_CODE error);
 
 // Report a fatal error and reset the processor, if OS_error
 // returns. Used by the OS.
-void OS_report_fatal(OS_ERROR_CODE error);
+inline static void OS_report_fatal(OS_ERROR_CODE error);
 
 // Reset the processor as reaction to an error.
 void OS_error_reset_processor(void);
+
+
+
+//// implementation
+
+// Those functions are short 'inline static' functions for performance reasons.
+// Therefore, they must be defined in the header file.
+
+#include "internal/Os_Kernel.h"
+
+inline static void OS_report_error(OS_ERROR_CODE error) {
+	OS_ENTER_CRITICAL();
+
+	OS_error(error);
+
+	OS_EXIT_CRITICAL();
+}
+
+inline static void OS_report_fatal(OS_ERROR_CODE error) {
+	cli();
+
+	OS_error(error);
+
+	OS_error_reset_processor();
+}
 
 #endif /* OS_ERROR_H_ */
