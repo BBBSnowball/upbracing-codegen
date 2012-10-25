ff2c9b3a0806dcc9317a015bed48427796aaa838
Sven Schönberg <sven_schoenberg@yahoo.de>
2012-10-26 01:16:56 +0200

    Created small test for problem with tasks.

--- code-generation/CAN-test-AVR/CAN-test-AVR.c
+++ code-generation/CAN-test-AVR/CAN-test-AVR.c
@@ -5,43 +5,46 @@
  *      Author: sven
  */
 
-#include <avr/io.h>
 #include <util/delay.h>
+
+#include <avr/io.h>
 #include <avr/pgmspace.h>
 #include <avr/interrupt.h>
-#include "gen/global_variables.h"
+#include <avr/wdt.h>
 
 #include "Os.h"
-#include "drivers/USART.h"
 #include "drivers/Gpio.h"
 #include "semaphores/semaphore.h"
+#include "internal/Os_Error.h"
 
 #include "gen/can.h"
+#include "gen/global_variables.h"
 #include "gen/Os_cfg_application.h"
 
+#include "rs232.h"
+
 int main(void) {
-	// OS things
+	// Init the USART (9600 8N1)
+	usart_init();
+
 	PORTA = 0x01;
 
-	// Init GPIO: (demo: DDRA = 0xFF)
-	GpioInit();
+	_delay_ms(1000);
+
+	usart_send_str("Startup\n");
 
 	PORTA = 0x02;
 
-	// Init the USART (9600 8N1)
-	USARTInit(51);
-
-	PORTA = 0x04;
-
-	USARTEnqueue(6,"Hallo\n");
-
-	PORTA = 0x08;
-
-	can_init_mobs();
-
-	PORTA = 0x10;
+	//can_init_mobs();
 
 	StartOS();
 
 	while(1);
+}
+
+TASK(Task_Bla) {
+
+	usart_send_str("Task Bla\n");
+
+	TerminateTask();
 }
--- code-generation/CAN-test-AVR/config.rb
+++ code-generation/CAN-test-AVR/config.rb
@@ -2,22 +2,19 @@ puts "Hello from config.rb"
 
 puts Dir.pwd
 
-require 'parse-dbc.rb'
-require 'parse-ecu-list.rb'
-
 ecus = read_ecu_list("ecu-list-cantest.xml")
 $config.ecus = ecus
 
-$config.can = parse_dbc("can_test.dbc")
+# $config.can = parse_dbc("can_test.dbc")
 
 $config.selectEcu("Cockpit")
 
 $config.rtos.clock = 8000000
-$config.rtos.tick_frequency = 250
+$config.rtos.tick_frequency = 640
 $config.rtos.conformance_class = "BCC1"
 
-task_usart = $config.rtos.addTask("UsartTransmit", SUSPENDED, 5)
+$config.rtos.addTask("Bla", SUSPENDED, 640)
 
-$config.canConfig.getMessage("Logger").period = "10ms"
-$config.canConfig.getMessage("Ignition").period = 0.01
-$config.canConfig.getMessage("Start").period = "0.5s"
+# $config.canConfig.getMessage("Logger").period = "10ms"
+# $config.canConfig.getMessage("Ignition").period = 0.01
+# $config.canConfig.getMessage("Start").period = "0.5s"
