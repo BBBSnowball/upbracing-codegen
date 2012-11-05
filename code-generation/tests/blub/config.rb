5fd55c0cf905d2713a682c086a253336344ff268
benny <bbbsnowball@gmail.com>
2012-11-05 01:13:43 +0100

    simple test case for LEDs and buttons; some fixes and improvements for the Rakefile
    - fix F_CPU (16Mhz -> 8Mhz)
    - build_caRTOS rebuilds the library, if an OS source file changes
    - generator only run once (instead of for every configuration)
    - task "generate-all"
    - link_elf determines md5 of features header at a later time (when the task is run)
    - directory name can have a number prefix (for ordering)
    - tasks are sorted by directory name -> TODO Does rake call tasks in the order they are added?
    - build-NAME-CONFIGURATION tasks
    - name of test task includes the configuration
    - projects can be in a nested folder
    - projects must have a magic file: run-test.rb, config.rb or main.c

--- code-generation/tests/blub/config.rb
+++ code-generation/tests/blub/config.rb
@@ -1,6 +1,5 @@
 $config.rtos.clock = 8000000
 $config.rtos.tick_frequency = 5 #250
-$config.rtos.conformance_class = "BCC1"
 
 task_update = $config.rtos.addTask("Update", SUSPENDED, 1)
 task_increment = $config.rtos.addTask("Increment", SUSPENDED, 5)
