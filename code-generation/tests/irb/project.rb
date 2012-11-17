ff316f9d3a9d7ca9ee2373490bc39bc0a5234fa2
benny <bbbsnowball@gmail.com>
2012-11-17 02:00:15 +0100

    make sure that some dummy tests aren't run by default (by test-all)

--- code-generation/tests/irb/project.rb
+++ code-generation/tests/irb/project.rb
@@ -2,6 +2,13 @@ $project = Project.new
 
 class << $project
     def get_test_dependencies(project_dir, test_helper)
+    	# don't build anything
+    	# (super method would return the hex file)
         []
+    end
+
+    def add_to_test_all(dir, configuration, test)
+    	# we don't want to run this for test-all
+    	# -> do nothing
     end
 end
