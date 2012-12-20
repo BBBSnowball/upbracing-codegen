5d0b64a14ec9087dfa7490ce29df5ccce565d5dd
Sven Schönberg <sven34>
2012-12-20 02:49:48 +0100

    Added can_at90.c to project.rb, so it is linked automatically by the rakefile. Modified rakefile to handle source files with ../../ directories correctly.

--- code-generation/tests/0015-can/project.rb
+++ code-generation/tests/0015-can/project.rb
@@ -3,7 +3,12 @@ class MyProject < ProjectWithCommon
 	def initialize
 		super
 		add_common :rs232
+    @custom_source_files = ["#/../../avr-programs/upbracing-common/can_at90.c"]
 	end
+	
+  def additional_source_files(source_dir, name, configuration)
+    @custom_source_files + super
+  end
 end
 
 $project = MyProject.new
