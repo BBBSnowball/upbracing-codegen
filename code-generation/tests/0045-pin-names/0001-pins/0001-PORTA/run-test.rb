9010a2ce7d38bc7c0035c8c64d0447b5d34727c8
rishab <rishabdhar1988@gmail.com>
2012-12-29 18:15:44 +0100

    minor modification to run-test.rb file

--- code-generation/tests/0045-pin-names/0001-pins/0001-PORTA/run-test.rb
+++ code-generation/tests/0045-pin-names/0001-pins/0001-PORTA/run-test.rb
@@ -7,10 +7,10 @@ same LED pattern on the board as is shown in the console.");
 
 $toolkit.showInstruction("X means that the LED is on and - means that the LED is off. \r\n
 The LED layout is like this : \r\n
-8 7 6 5 4 3 2 1 \r\n
+ 7 6 5 4 3 2 1 0 \r\n
 The leftmost LED on the board is the leftmost LED on the console and viceversa \r\n
 For example: If you see pattern -X------ on the console only the 7th LED \r\n
-on the board must be on");
+on the board must be on.");
 
 #flash the program onto the microcontroller
 $helper.flash_processor
