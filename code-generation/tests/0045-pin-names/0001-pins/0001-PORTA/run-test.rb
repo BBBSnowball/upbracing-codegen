282d004e0d578df2e2be1bcc8604b9f1ce5349bc
benny <bbbsnowball@gmail.com>
2012-12-29 17:57:02 +0100

    code-generation - tests: more changes to make it run without errors

--- code-generation/tests/0045-pin-names/0001-pins/0001-PORTA/run-test.rb
+++ code-generation/tests/0045-pin-names/0001-pins/0001-PORTA/run-test.rb
@@ -2,10 +2,10 @@
 $helper.erase_processor
 
 #show instructions to the user 
-$toolkit.showInstruction("This test blinks some LEDs and you would be asked to answer whether you see the 
+$toolkit.showInstructions("This test blinks some LEDs and you would be asked to answer whether you see the 
 same LED pattern on the board as is shown in the console.");
 
-$toolkit.showInstruction("X means that the LED is on and - means that the LED is off. \r\n
+$toolkit.showInstructions("X means that the LED is on and - means that the LED is off. \r\n
 The LED layout is like this : \r\n
 8 7 6 5 4 3 2 1 \r\n
 The leftmost LED on the board is the leftmost LED on the console and viceversa \r\n
