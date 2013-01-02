ab75df2b7215968a853ac42662e61d1648644103
rishab <rishabdhar1988@gmail.com>
2013-01-03 00:14:16 +0100

    compasskeyboard tests updated to make them work

--- code-generation/tests/0045-pin-names/0001-pins/0001-Compasskeyboard/main.c
+++ code-generation/tests/0045-pin-names/0001-pins/0001-Compasskeyboard/main.c
@@ -135,24 +135,13 @@ int main() {
 				char is_set = usart_recv();
 
 				while (is_set != '0') {
-					if (is_set == 'a') {
-						if (!IS_SET(LOW_FUEL))
-							usart_send_str("\r\n");
-					}
-
-					if (is_set == 'b') {
-						if (IS_SET(LOW_FUEL))
-							usart_send_str("\r\n");
-					}
-
-					if (is_set == 'c') {
-						if ((!IS_SET(ENGINE_FAILURE))
-								&& (!IS_SET(HEADLIGHT_NOT_WORKING))
-								&& (!IS_SET(FAULTY_EXHAUST)) && (!IS_SET(HIGH_TEMP))
-								&& (!IS_SET(CHANGE_GEAR)))
-
-							usart_send_str("\r\n");
-					}
+					case 'a': REGISTER_DATA("PORTE.2", IS_SET(LOW_FUEL), 16, 2);
+					case 'b': REGISTER_DATA("PORTE.2", IS_SET(LOW_FUEL), 16, 2);
+					case 'c':{REGISTER_DATA("PORTD.1", IS_SET(CHANGE_GEAR), 16, 2);
+							  REGISTER_DATA("PORTE.4", IS_SET(ENGINE_FAILURE), 16, 2);
+							  REGISTER_DATA("PORTE.5", IS_SET(HEADLIGHT_NOT_WORKING), 16, 2);
+							  REGISTER_DATA("PORTE.6", IS_SET(FAULTY_EXHAUST), 16, 2);
+							  REGISTER_DATA("PORTE.7", IS_SET(HIGH_TEMP), 16, 2);}
 
 					is_set = usart_recv();
 				}
