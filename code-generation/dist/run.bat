63adcf934bf0686d93fc7e2e2515a21fcc9740f6
benny <bbbsnowball@gmail.com>
2013-03-19 22:59:05 +0100

    Modify run scripts in distribution, so we can use a "fat" JAR. The other approach ("export required libraries into generated JAR") might conflict with the license of some libraries.

--- code-generation/dist/run.bat
+++ code-generation/dist/run.bat
@@ -1,3 +1,3 @@
 @echo off
 
-java -cp "%~dp0\de.upbracing.code_generation.jar" de.upbracing.code_generation.Main %*
+java -cp "%~dp0\de.upbracing.code_generation.jar" org.eclipse.jdt.internal.jarinjarloader.JarRsrcLoader %*
